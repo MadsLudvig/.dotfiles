e13df16e17e4b54e87e1de53cd5e0c351098427e
Mads Ludvig Timm Fagerlund <mads@fagerlund.io>
2024-06-23 10:08:10 +0200

    add typescript and svelte

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -14,7 +14,9 @@ return {
       formatters_by_ft = {
         lua = { "stylua" },
         javascript = { { "prettier" } },
+        typescript = { { "prettier" } },
         typescriptreact = { { "prettier" } },
+        svelte = { { "prettier" } },
       },
     },
   },
