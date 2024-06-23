3d5c934d5406012cf3cf9b6f921fd7ba3fe6d97a
Mads Ludvig Timm Fagerlund <mads@fagerlund.io>
2024-06-23 10:08:25 +0200

    add comment

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -5,6 +5,7 @@ return {
     config = function()
       require("mini.ai").setup({ n_lines = 500 })
       require("mini.pairs").setup()
+      -- require("mini.surround").setup()
       require("mini.comment").setup()
     end,
   },
