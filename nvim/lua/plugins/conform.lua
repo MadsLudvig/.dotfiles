2aee8bd9a01e550c07331e77f09e8fc5459583dc
Mads Fagerlund <mads@fagerlund.io>
2025-03-30 13:43:33 +0200

    configure after newest standards

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,23 +1,22 @@
 return {
-  { -- Autoformat
+  {
     "stevearc/conform.nvim",
     event = "VimEnter",
     opts = {
       notify_on_error = true,
-      format_on_save = function(bufnr)
-        local disable_filetypes = { c = true, cpp = true }
-        return {
-          timeout_ms = 500,
-          lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-        }
-      end,
+      format_on_save = {
+        timeout_ms = 50,
+        lsp_fallback = function(bufnr)
+          return not disable_filetypes[vim.bo[bufnr].filetype]
+        end,
+      },
       formatters_by_ft = {
         lua = { "stylua" },
-        javascript = { { "prettier" } },
-        typescript = { { "prettier" } },
-        typescriptreact = { { "prettier" } },
-        svelte = { { "prettier" } },
-        xml = { { "xmlformat" } },
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        typescriptreact = { "prettier" },
+        svelte = { "prettier" },
+        xml = { "xmlformat" },
       },
     },
   },
