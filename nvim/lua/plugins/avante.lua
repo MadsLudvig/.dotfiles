587b6b695c67f72d49f14aa023f1300db388f9d2
Mads Fagerlund <mads@fagerlund.io>
2025-03-30 13:42:38 +0200

    remove unescessary code

--- nvim/lua/plugins/avante.lua
+++ nvim/lua/plugins/avante.lua
@@ -6,26 +6,14 @@ return {
   opts = {
     provider = "copilot",
     auto_suggestions_provider = "copilot",
-    copilot = {
-      model = "o3-mini",
-    },
-    -- suggestion = {
-    --   debounce = 600,
-    --   throttle = 600,
-    -- },
   },
-  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   build = "make",
-  -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
   dependencies = {
     "stevearc/dressing.nvim",
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
-    --- The below dependencies are optional,
     "echasnovski/mini.pick", -- for file_selector provider mini.pick
-    "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
     "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
-    "ibhagwan/fzf-lua", -- for file_selector provider fzf
     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
     "zbirenbaum/copilot.lua", -- for providers='copilot'
     {
