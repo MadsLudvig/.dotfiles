2c8b27e7b72a850f5d202649103151e8e85c1a38
Mads Fagerlund <mads@fagerlund.io>
2025-03-30 13:43:46 +0200

    set copilot model to newest claude-3-7-sonnet

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -4,5 +4,6 @@ return {
     -- Optional: Configure Copilot settings
     vim.g.copilot_no_tab_map = true -- Prevent Copilot from mapping the Tab key
     vim.api.nvim_set_keymap("i", "<C-j>", 'copilot#Accept("<CR>")', { expr = true, silent = true })
+    vim.g.copilot_model = "claude-3-7-sonnet"
   end,
 }
