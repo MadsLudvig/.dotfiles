60333341984c1576fe8a77d4813e33a99b81af48
Mads Fagerlund <mads@fagerlund.io>
2025-03-30 13:49:43 +0200

    reconfigure which-key to be updated to newest spec

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -1,25 +1,25 @@
 return {
-  { -- Useful plugin to show you pending keybinds.
+  {
     "folke/which-key.nvim",
-    event = "VeryLazy", -- Sets the loading event to 'VimEnter'
-    opts = {
-      spec = {
-        -- Groups
-        { lhs = "g", group = "goto" }, -- "g" mappings group
-        { lhs = "<leader>b", group = "buffer" }, -- "buffer" group
-        { lhs = "<leader>c", group = "code" }, -- "code" group
-        { lhs = "<leader>f", group = "file/find" }, -- "file/find" group
-        { lhs = "<leader>g", group = "git" }, -- "git" group
-        { lhs = "<leader>q", group = "quit/session" }, -- "quit/session" group
-        { lhs = "<leader>s", group = "search" }, -- "search" group
-        { lhs = "<leader>d", group = "debug" }, -- "debug" group
-        { lhs = "<leader>u", group = "ui" }, -- "ui" group
-        { lhs = "<leader>w", group = "windows" }, -- "windows" group
-        { lhs = "<leader>t", group = "trouble" }, -- "trouble" group
-      },
-    },
-    config = function() -- This is the function that runs, AFTER loading
-      require("which-key").setup()
+    event = "VeryLazy",
+    keys = { "<leader>" },
+    config = function()
+      local wk = require("which-key")
+      wk.add({
+        { "<leader>g", group = "goto" }, -- group
+        { "<leader>o", group = "octo" }, -- group
+        { "<leader>b", group = "buffer" }, -- group
+        { "<leader>c", group = "code" }, -- group
+        { "<leader>f", group = "file/find" }, -- group
+        { "<leader>g", group = "git" }, -- group
+        { "<leader>q", group = "quit/session" }, -- group
+        { "<leader>s", group = "search" }, -- group
+        { "<leader>d", group = "debug" }, -- group
+        { "<leader>u", group = "ui" }, -- group
+        { "<leader>w", group = "windows" }, -- group
+        { "<leader>t", group = "trouble" }, -- group
+      })
+      wk.setup()
     end,
   },
 }
